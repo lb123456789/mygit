ea08c458ebddd040c706480c7fc738691d5d20f7
Administrator <490251568@qq.com>
2019-05-09 13:59:24 +0800

    界面切换

---
+++ mainUi.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'mainUi.ui'
+#
+# Created by: PyQt5 UI code generator 5.11.3
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(1099, 773)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.formLayoutWidget = QtWidgets.QWidget(self.centralwidget)
+        self.formLayoutWidget.setGeometry(QtCore.QRect(370, 320, 351, 141))
+        self.formLayoutWidget.setObjectName("formLayoutWidget")
+        self.formLayout = QtWidgets.QFormLayout(self.formLayoutWidget)
+        self.formLayout.setContentsMargins(0, 0, 0, 0)
+        self.formLayout.setObjectName("formLayout")
+        self.labeltext1 = QtWidgets.QLabel(self.formLayoutWidget)
+        font = QtGui.QFont()
+        font.setPointSize(15)
+        self.labeltext1.setFont(font)
+        self.labeltext1.setObjectName("labeltext1")
+        self.formLayout.setWidget(0, QtWidgets.QFormLayout.LabelRole, self.labeltext1)
+        self.lineEdit1 = QtWidgets.QLineEdit(self.formLayoutWidget)
+        font = QtGui.QFont()
+        font.setPointSize(15)
+        self.lineEdit1.setFont(font)
+        self.lineEdit1.setObjectName("lineEdit1")
+        self.formLayout.setWidget(0, QtWidgets.QFormLayout.FieldRole, self.lineEdit1)
+        self.labeltext2 = QtWidgets.QLabel(self.formLayoutWidget)
+        font = QtGui.QFont()
+        font.setPointSize(15)
+        self.labeltext2.setFont(font)
+        self.labeltext2.setObjectName("labeltext2")
+        self.formLayout.setWidget(1, QtWidgets.QFormLayout.LabelRole, self.labeltext2)
+        self.lineEdit2 = QtWidgets.QLineEdit(self.formLayoutWidget)
+        font = QtGui.QFont()
+        font.setPointSize(15)
+        self.lineEdit2.setFont(font)
+        self.lineEdit2.setObjectName("lineEdit2")
+        self.formLayout.setWidget(1, QtWidgets.QFormLayout.FieldRole, self.lineEdit2)
+        self.pushButton1 = QtWidgets.QPushButton(self.formLayoutWidget)
+        font = QtGui.QFont()
+        font.setPointSize(15)
+        self.pushButton1.setFont(font)
+        self.pushButton1.setObjectName("pushButton1")
+        self.formLayout.setWidget(2, QtWidgets.QFormLayout.FieldRole, self.pushButton1)
+        self.labeltext4_2 = QtWidgets.QLabel(self.centralwidget)
+        self.labeltext4_2.setGeometry(QtCore.QRect(350, 20, 431, 151))
+        font = QtGui.QFont()
+        font.setPointSize(25)
+        self.labeltext4_2.setFont(font)
+        self.labeltext4_2.setObjectName("labeltext4_2")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 1099, 26))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.labeltext1.setText(_translate("MainWindow", "用户名"))
+        self.labeltext2.setText(_translate("MainWindow", "密 码"))
+        self.pushButton1.setText(_translate("MainWindow", "保存"))
+        self.labeltext4_2.setText(_translate("MainWindow", "齿轮加工制造生产线"))
+
---
+++ mainUione.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'mainUione.ui'
+#
+# Created by: PyQt5 UI code generator 5.11.3
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(1099, 773)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.labeltext4_2 = QtWidgets.QLabel(self.centralwidget)
+        self.labeltext4_2.setGeometry(QtCore.QRect(410, 20, 431, 151))
+        font = QtGui.QFont()
+        font.setPointSize(25)
+        self.labeltext4_2.setFont(font)
+        self.labeltext4_2.setObjectName("labeltext4_2")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 1099, 26))
+        self.menubar.setObjectName("menubar")
+        self.menu = QtWidgets.QMenu(self.menubar)
+        self.menu.setObjectName("menu")
+        self.menu_2 = QtWidgets.QMenu(self.menubar)
+        self.menu_2.setObjectName("menu_2")
+        self.menu_3 = QtWidgets.QMenu(self.menubar)
+        self.menu_3.setObjectName("menu_3")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+        self.menubar.addAction(self.menu.menuAction())
+        self.menubar.addAction(self.menu_2.menuAction())
+        self.menubar.addAction(self.menu_3.menuAction())
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.labeltext4_2.setText(_translate("MainWindow", "自动加工界面"))
+        self.menu.setTitle(_translate("MainWindow", "文件"))
+        self.menu_2.setTitle(_translate("MainWindow", "编辑"))
+        self.menu_3.setTitle(_translate("MainWindow", "设置"))
+
