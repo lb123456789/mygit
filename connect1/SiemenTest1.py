2128d82a4794fc7bec304120f0de532e9c428a2c
Administrator <490251568@qq.com>
2019-05-28 18:06:39 +0800

    20190228

--- connect1/SiemenTest1.py
+++ connect1/SiemenTest1.py
@@ -4,40 +4,11 @@ sys.path.append('.\HslCommunication_Python')
 from HslCommunication_Python.HslCommunication import SiemensS7Net
 from HslCommunication_Python.HslCommunication import SiemensPLCS
 from HslCommunication_Python.HslCommunication import SoftBasic
-import os
-import  time
-from multiprocessing import Pool
-from multiprocessing import Process
 
-"西门子读写类"
+siemens = SiemensS7Net(SiemensPLCS.S1200, "127.0.0.1")
 
-class NewProcess(Process,): #继承Process类创建一个新类
-    def __init__(self,num,adrss,IP):
-        self.num = num
-        self.adrss = adrss
-        super().__init__()
-        self.siemens = SiemensS7Net(SiemensPLCS.S1200,IP)
-        self.aa = 1
+if siemens.ConnectServer().IsSuccess == False:
+        print("connect falied")
 
-    def run(self):  #重写Process类中的run方法.
-        while True:
-
-            read = self.siemens.ReadInt16(self.adrss,10000)
-            AA =self.aa
-            print(str(read.Content))
-            print(self.aa)
-            self.aa=self.aa+1
-            #print('我是进程%d,我的pid是:%d'%(self.num,os.getpid()))
-            time.sleep(1)
-
-if __name__ == '__main__':
-    #p = NewProcess(1,"Q","192.168.9.57")
-    p1 = NewProcess(2,"M100","127.0.0.1")
-   # p.start()
-    p1.start()
-
-
-
-
-
-
+        # int16 read write test
+        siemens.WriteInt16("M102", 12358)
--- yufa.py
+++ yufa.py
@@ -42,3 +42,89 @@ def power(x, n):
         return x* power(x,n-1)
 print(power(2, 3))
 """
+
+import random
+from abc import abstractmethod, ABCMeta
+
+#------产品------
+
+class Player:
+    def __init__(self, face=None, body=None, arm=None, leg=None):
+        self.face = face
+        self.arm = arm
+        self.leg = leg
+        self.body = body
+
+    def __str__(self):
+        return "%s, %s, %s, %s" % (self.face, self.arm, self.body, self.leg)
+
+
+#------建造者------
+
+
+class PlayerBuilder(metaclass=ABCMeta):
+    @abstractmethod
+    def build_face(self):
+        pass
+    @abstractmethod
+    def build_arm(self):
+        pass
+    @abstractmethod
+    def build_leg(self):
+        pass
+    @abstractmethod
+    def build_body(self):
+        pass
+    @abstractmethod
+    def get_player(self):
+        pass
+
+
+class BeautifulWomanBuilder(PlayerBuilder):
+    def __init__(self):
+        self.player = Player()
+    def build_face(self):
+        self.player.face = "漂亮脸蛋"
+    def build_arm(self):
+        self.player.arm="细胳膊"
+    def build_body(self):
+        self.player.body="细腰"
+    def build_leg(self):
+        self.player.leg="长腿"
+    def get_player(self):
+        return self.player
+
+class RandomPlayerBuilder(PlayerBuilder):
+    def __init__(self):
+        self.player = Player()
+    def build_face(self):
+        self.player.face = random.choice(["瓜子脸","西瓜子脸"])
+    def build_arm(self):
+        self.player.arm=random.choice(["长胳膊","短胳膊"])
+    def build_body(self):
+        self.player.body=random.choice(["苗条","胖"])
+    def build_leg(self):
+        self.player.leg=random.choice(["长腿","短腿"])
+    def get_player(self):
+        return self.player
+
+class PlayerDirector:
+    def __init__(self, builder):
+        self.builder = builder
+    # 控制组装顺序
+    def build_player(self):
+        self.builder.build_body()
+        self.builder.build_face()
+        self.builder.build_arm()
+        self.builder.build_leg()
+        return self.builder.get_player()
+
+
+
+
+pd = PlayerDirector(RandomPlayerBuilder())
+p = pd.build_player()
+print(p)
+
+print(pd.build_player())
+print(pd.build_player)
